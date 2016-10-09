0ee77531a30fe513a30ac4de4902c24058ef4252
David Jablonski <jablo36@yahoo.com>
2016-10-09 02:58:54 -0700

    Update hive_base_ddl.sql

--- exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -1,2 +1,139 @@
 
 
+CREATE EXTERNAL TABLE hospital_info (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’; 
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+
+
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES ( 
+   "separatorChar" = ",", 
+   "quoteChar"     = '"', 
+   "escapeChar"    = '\\' 
+) 
+STORED AS TEXTFILE 
+LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
