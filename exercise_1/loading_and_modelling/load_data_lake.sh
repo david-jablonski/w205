753f1f9acea4488866f6ff4df9f3d80278312ffd
David Jablonski <jablo36@yahoo.com>
2016-10-09 02:34:29 -0700

    Update load_data_lake.sh

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,1 +1,12 @@
+/root/start-hadoop.sh
+/data/start_postgres.sh
+su - w205
+hdfs dfs -mkdir /user/w205/exercise_1
+wget https://github.com/david-jablonski/w205/blob/master/exercise_1/loading_and_modelling/import_hospital_files/Hospital_Revised_Flatfiles.zip
+hdfs dfs -put Hospital_Revised_Flatfiles.zip /user/w205/exercise_1/loading_and_modelling
+tail -n +2 /path/to/original > /path/to/new_file
+tail -n +2 /path/to/original > /path/to/new_file
+tail -n +2 /path/to/original > /path/to/new_file
+tail -n +2 /path/to/original > /path/to/new_file
+tail -n +2 /path/to/original > /path/to/new_file
 
