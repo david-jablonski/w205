5ddab51c98ed5d1b69eec843b9f8d79e673fd269
David Jablonski <jablo36@yahoo.com>
2016-10-09 22:27:20 -0700

    Update hive_base_ddl.sql

--- exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -1,6 +1,7 @@
-
-
-CREATE EXTERNAL TABLE hospital_info (<col1_name>, <col1_type>, …) 
+--CREATING TABLE FOR HOSPITAL INFORMATION
+CREATE EXTERNAL TABLE hospital_info (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, hospital_type STRING, 
+                                     hospital_ownership STRING, emergency_services STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -8,10 +9,12 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’; 
+LOCATION '/user/w205/hospital_compare/hospital_info'; 
 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+--CREATING TABLE FOR Measures info
+CREATE EXTERNAL TABLE measures (measure_name STRING, measure_id STRING, measure_start_quater STRING, measure_start_date STRING, 
+                                     measure_end_quarter STRING, measure_end_date STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -19,9 +22,15 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/measures'; 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+
+--CREATING TABLE FOR effective_care_h
+CREATE EXTERNAL TABLE effective_care_h (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, condtion STRING, 
+                                     measure_id STRING, score STRING, sample STRING, footnote STRING, 
+                                     measure_start_date STRING, measure_end_date STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -29,9 +38,13 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/effective_care_h'; 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+
+--CREATING TABLE FOR effective_care_s
+CREATE EXTERNAL TABLE effective_care_s (state STRING, condtion STRING, measure_name STRING, measure_id STRING,
+                                     score STRING, footnote STRING, measure_start_date STRING, measure_end_date STRING)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -39,9 +52,15 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/effective_care_s'; 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+
+--CREATING TABLE FOR complications_h
+CREATE EXTERNAL TABLE complications_h (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, measure_name STRING, measure_id STRING, 
+                                     compared_to_national STRING, denominator STRING, score STRING, lower_estimate STRING, higher_estimate STRING, 
+                                     footnote STRING, measure_start_date STRING, measure_end_date STRING) 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -49,9 +68,14 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/complications_h'; 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+
+--CREATING TABLE FOR complications_s
+CREATE EXTERNAL TABLE complications_s (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, hospital_type STRING, 
+                                     hospital_ownership STRING, emergency_services STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -59,10 +83,14 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/complications_s'; 
 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+--CREATING TABLE FOR infections_h
+CREATE EXTERNAL TABLE infections_h (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, hospital_type STRING, 
+                                     hospital_ownership STRING, emergency_services STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -70,10 +98,12 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/infections_h'; 
 
-
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+--CREATING TABLE FOR infections_s
+CREATE EXTERNAL TABLE infections_s (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, hospital_type STRING, 
+                                     hospital_ownership STRING, emergency_services STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -81,10 +111,14 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/infections_s'; 
 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+--CREATING TABLE FOR imaging_h
+CREATE EXTERNAL TABLE imaging_h (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, hospital_type STRING, 
+                                     hospital_ownership STRING, emergency_services STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -92,10 +126,14 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/imaging_h'; 
 
 
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
+
+--CREATING TABLE FOR imaging_s
+CREATE EXTERNAL TABLE imaging_s (provider_id STRING, hospital_name STRING, address STRING, city STRING, state STRING, 
+                                     zip_code STRING, county_name STRING, phone_number STRING, hospital_type STRING, 
+                                     hospital_ownership STRING, emergency_services STRING)  
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
 WITH SERDEPROPERTIES ( 
    "separatorChar" = ",", 
@@ -103,37 +141,4 @@ WITH SERDEPROPERTIES (
    "escapeChar"    = '\\' 
 ) 
 STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
-
-
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( 
-   "separatorChar" = ",", 
-   "quoteChar"     = '"', 
-   "escapeChar"    = '\\' 
-) 
-STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
-
-
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( 
-   "separatorChar" = ",", 
-   "quoteChar"     = '"', 
-   "escapeChar"    = '\\' 
-) 
-STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
-
-
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …) 
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( 
-   "separatorChar" = ",", 
-   "quoteChar"     = '"', 
-   "escapeChar"    = '\\' 
-) 
-STORED AS TEXTFILE 
-LOCATION ‘/user/w205/exercise_1/loading_and_modelling’;
+LOCATION '/user/w205/hospital_compare/imaging_s'; 
