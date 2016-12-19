b8fbf8ee7654e280aa9bc9f5eaf0df4ed5bc7e48
David Jablonski <jablo36@yahoo.com>
2016-12-19 12:50:06 -0800

    Update best_hospitals.sql

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,9 +1,8 @@
 --SQL file to get the 10 best hospitals
 --Transforming TABLE FOR effective_care_h
-CREATE TABLE effective_care_h_2 as select provider_id, hospital_name, measure_id, score from effective_care_h;
-CREATE TABLE effective_care_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from effective_care_h_2;
-CREATE TABLE effective_care_h_4 as select * from effective_care_h_3 where score_num <= 1000 and length(score) < 6;
-CREATE TABLE effective_care_h_5 as select *, SUM(score_num) as total_score from effective_care_h_4;
 
-
-SELECT * FROM effective_care_h_5 ORDER BY total_score DESC LIMIT 10;
+select *, SUM(score_num) as total_score 
+from effective_care_h_4
+group by provider_id
+order by SUM(score_num) desc
+limit 10;
