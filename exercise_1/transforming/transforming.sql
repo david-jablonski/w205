303e743a5b78c18c8e69cb8b02ffe9fea8903a76
David Jablonski <jablo36@yahoo.com>
2016-12-19 10:48:07 -0800

    Update transforming.sql

--- exercise_1/transforming/transforming.sql
+++ exercise_1/transforming/transforming.sql
@@ -1,53 +1,59 @@
 --Transforming TABLE FOR effective_care_h
 CREATE TABLE effective_care_h_2 as select provider_id, hospital_name, measure_id, score from effective_care_h;
-UPDATE effective_care_h_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE effective_care_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from effective_care_h_2;
+CREATE TABLE effective_care_h_4 as select * from effective_care_h_3 where score_num <= 1000 and length(score) < 6;
+
 
 --Transforming TABLE FOR effective_care_s
 CREATE TABLE effective_care_s_2 as select state, measure_id, score from effective_care_s;
-UPDATE effective_care_s_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE effective_care_s_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from effective_care_s_2;
+CREATE TABLE effective_care_s_4 as select * from effective_care_s_3 where score_num <= 1000 and length(score) < 6;
+
 
 --Transforming TABLE FOR complications_h
 CREATE TABLE complications_h_2 as select provider_id, hospital_name, measure_id, score from complications_h;
-UPDATE complications_h_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE complications_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from complications_h_2;
+CREATE TABLE complications_h_4 as select * from complications_h_3 where score_num <= 250 and length(score) < 6;
+
 
 --Transforming TABLE FOR complications_s
 CREATE TABLE complications_s_2 as select state, measure_id, number_of_hospitals_worse, number_of_hospitals_same, number_of_hospitals_better, number_of_hospitals_too_few from complications_s;
-UPDATE complications_s_2 SET number_of_hospitals_worse = '0' WHERE score = 'Not Available';
-UPDATE complications_s_2 SET number_of_hospitals_same = '0' WHERE score = 'Not Available';
-UPDATE complications_s_2 SET number_of_hospitals_better = '0' WHERE score = 'Not Available';
-UPDATE complications_s_2 SET number_of_hospitals_too_few = '0' WHERE score = 'Not Available';
+CREATE TABLE complications_h_3 as select *, regexp_extract(number_of_hospitals_better, '[0-9]+', 0) as score_num from complications_h_2;
+CREATE TABLE complications_h_4 as select * from complications_h_3 where score_num <= 20 and length(score) < 6;
+
 
 --Transforming TABLE FOR imaging_h
 CREATE TABLE imaging_h_2 as select provider_id, hospital_name, measure_id, score from imaging_h;
-UPDATE imaging_h_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE imaging_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from imaging_h_2;
+CREATE TABLE imaging_h_4 as select * from imaging_h_3 where score_num <= 100 and length(score) < 6;
+
 
 --Transforming TABLE FOR imaging_s
 CREATE TABLE imaging_s_2 as select state, measure_id, score from imaging_s;
-UPDATE imaging_s_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE imaging_s_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from imaging_s_2;
+CREATE TABLE imaging_s_4 as select * from imaging_s_3 where score_num <= 100 and length(score) < 6;
+
 
 --Transforming TABLE FOR infections_h
 CREATE TABLE infections_h_2 as select provider_id, hospital_name, measure_id, score from infections_h;
-UPDATE infections_h_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE infections_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from infections_h_2;
+CREATE TABLE infections_h_4 as select * from infections_h_3 where score_num <= 10 and length(score) < 6;
+
 
 --Transforming TABLE FOR infections_s
 CREATE TABLE infections_s_2 as select state, measure_id, score from infections_s;
-UPDATE infections_s_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE infections_s_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from infections_s_2;
+CREATE TABLE infections_s_4 as select * from infections_s_3 where score_num <= 10 and length(score) < 6;
+
 
 --Transforming TABLE FOR readmissions_h
 CREATE TABLE readmissions_h_2 as select provider_id, hospital_name, measure_id, score from readmissions_h;
-UPDATE readmissions_h_2 SET score = '0' WHERE score = 'Not Available';
+CREATE TABLE readmissions_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from readmissions_h_2;
+CREATE TABLE readmissions_h_4 as select * from readmissions_h_3 where score_num <= 40 and length(score) < 6;
+
 
 --Transforming TABLE FOR readmissions_s
 CREATE TABLE readmissions_s_2 as select state, measure_id, number_of_hospitals_worse, number_of_hospitals_same, number_of_hospitals_better, number_of_hospitals_too_few from readmissions_s;
-UPDATE readmissions_s_2 SET number_of_hospitals_worse = '0' WHERE score = 'Not Available';
-UPDATE readmissions_s_2 SET number_of_hospitals_same = '0' WHERE score = 'Not Available';
-UPDATE readmissions_s_2 SET number_of_hospitals_better = '0' WHERE score = 'Not Available';
-UPDATE readmissions_s_2 SET number_of_hospitals_too_few = '0' WHERE score = 'Not Available';
-
-
-
-
-create table newtable as select column1, column2, column3 from oldtable
-
-
+CREATE TABLE readmissions_s_3 as select *, regexp_extract(number_of_hospitals_better, '[0-9]+', 0) as score_num from readmissions_s_2;
+CREATE TABLE readmissions_s_4 as select * from readmissions_s_3 where score_num <= 10 and length(score) < 6;
 
