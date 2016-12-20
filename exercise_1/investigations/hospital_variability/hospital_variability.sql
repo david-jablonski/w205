51fec8a8316630f423d92d12683365288979b8a8
David Jablonski <jablo36@yahoo.com>
2016-12-19 21:36:54 -0800

    Update hospital_variability.sql

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -1,5 +1,7 @@
 --SQL file for the top 100 hosiptal variability
-CREATE TABLE effective_care_h_2 as select provider_id, hospital_name, measure_id, score from effective_care_h;
-CREATE TABLE effective_care_h_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from effective_care_h_2;
-CREATE TABLE effective_care_h_4 as select * from effective_care_h_3 where score_num <= 1000 and length(score) < 6;
 
+select *, VARIANCE(score_num) as variance_score 
+from effective_care_s_4
+group by provider_id
+order by VARIANCE(score_num) desc
+limit 10;
