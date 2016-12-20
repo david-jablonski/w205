0884f8ef85f44292483336677cb038e8fcde7ed2
David Jablonski <jablo36@yahoo.com>
2016-12-20 14:38:01 -0800

    Update best_hospitals.sql

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,7 +1,8 @@
 --SQL file to get the 10 best hospitals
 --Transforming TABLE FOR effective_care_h
 
-select *, SUM(score_num) as total_score 
+CREATE TABLE best_hospitals 
+as select *, SUM(score_num) as total_score 
 from effective_care_h_4
 group by provider_id
 order by SUM(score_num) desc
