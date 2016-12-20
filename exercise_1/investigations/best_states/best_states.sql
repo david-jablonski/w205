5cbd258be0fdeefaa5086687e2bcc48044043a45
David Jablonski <jablo36@yahoo.com>
2016-12-20 14:59:49 -0800

    Update best_states.sql

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -1,10 +1,7 @@
---SQL file to get the 10 best states
+--SQL file to create a table to get the 10 best states
 
-CREATE TABLE effective_care_s_2 as select state, measure_id, score from effective_care_s;
-CREATE TABLE effective_care_s_3 as select *, regexp_extract(score, '[0-9]+', 0) as score_num from effective_care_s_2;
-CREATE TABLE effective_care_s_4 as select * from effective_care_s_3 where score_num <= 1000 and length(score) < 6;
-
-select *, SUM(score_num) as state_score 
+CREATE TABLE best_states
+as select *, SUM(score_num) as state_score 
 from effective_care_s_4
 group by state
 order by SUM(score_num) desc
