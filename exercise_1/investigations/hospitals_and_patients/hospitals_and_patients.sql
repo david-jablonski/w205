76f24b1ed7a07d1d615975f9a94fa5f09c5f34cb
David Jablonski <jablo36@yahoo.com>
2016-12-21 00:17:30 -0800

    Update hospitals_and_patients.sql

--- exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
+++ exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
@@ -3,13 +3,17 @@
 CREATE TABLE hospitals_score
 as select provider_id, hospital_name, SUM(score_num) as total_score 
 from effective_care_h_4
-group by provider_id, hospital_name
+group by provider_id, hospital_name;
 
 CREATE TABLE hospitals_and_patients
-as select total_score,  
-from effective_care_h_4, surveys_5
-where effective_care_h_4.provider_id = surveys_5.provider_id;
+as select total_score, score_total
+from hospitals_score, surveys_5
+where hospitals_score.provider_id = surveys_5.provider_id;
+
+CREATE TABLE hospitals_and_patients_2
+as select corr(total_score, score_total) as corr_score 
+from hospitals_and_patients;
 
 --To print chart for report
-select * from hospitals_and_patients;
+select corr_score from hospitals_and_patients_2;
 
