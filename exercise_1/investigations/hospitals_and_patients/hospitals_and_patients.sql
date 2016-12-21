f4cc87702c8bd3456736c32f7d974481bca6070d
David Jablonski <jablo36@yahoo.com>
2016-12-20 23:59:23 -0800

    Update hospitals_and_patients.sql

--- exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
+++ exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
@@ -1,5 +1,10 @@
 --SQL file hospitals_and_patients
 
+CREATE TABLE hospitals_score
+as select provider_id, hospital_name, SUM(score_num) as total_score 
+from effective_care_h_4
+group by provider_id, hospital_name
+
 CREATE TABLE hospitals_and_patients
 as select total_score,  
 from effective_care_h_4, surveys_5
