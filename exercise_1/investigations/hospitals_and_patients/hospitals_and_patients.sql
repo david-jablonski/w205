49e57fb1a85fc8da061b61faf29e4d06e782b5ee
David Jablonski <jablo36@yahoo.com>
2016-12-20 23:57:29 -0800

    Update hospitals_and_patients.sql

--- exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
+++ exercise_1/investigations/hospitals_and_patients/hospitals_and_patients.sql
@@ -1,1 +1,10 @@
 --SQL file hospitals_and_patients
+
+CREATE TABLE hospitals_and_patients
+as select total_score,  
+from effective_care_h_4, surveys_5
+where effective_care_h_4.provider_id = surveys_5.provider_id;
+
+--To print chart for report
+select * from hospitals_and_patients;
+
