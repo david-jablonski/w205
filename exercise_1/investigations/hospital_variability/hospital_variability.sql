eb80beba2eb8753f5545ef447b390a9409336afc
David Jablonski <jablo36@yahoo.com>
2016-12-20 22:59:25 -0800

    Update hospital_variability.sql

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -1,8 +1,11 @@
 --SQL file for the top 10 most variability among procedures in the different hospitals
 
-CREATE TABLE best_hospitals
-as select *, VARIANCE(score_num) as variance_score 
-from effective_care_s_4
+CREATE TABLE hospital_variability
+as select measure_id, VARIANCE(score_num) as variance_score 
+from effective_care_h_4
 group by measure_id
 order by variance_score desc
 limit 10;
+
+--To print chart for report
+select * from hospital_variability;
