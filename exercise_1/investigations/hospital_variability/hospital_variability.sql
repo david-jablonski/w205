8ddbf29bef768e5e0f02b4b425fb4041b487c365
David Jablonski <jablo36@yahoo.com>
2016-12-20 22:47:43 -0800

    Update hospital_variability.sql

--- exercise_1/investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability/hospital_variability.sql
@@ -4,5 +4,5 @@ CREATE TABLE best_hospitals
 as select *, VARIANCE(score_num) as variance_score 
 from effective_care_s_4
 group by measure_id
-order by VARIANCE(score_num) desc
+order by variance_score desc
 limit 10;
